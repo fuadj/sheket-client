15cdd13e16af5b9d86f58af1380d255936de9906
Fuad Seffa <fuadjemal@gmail.com>
2016-06-04 18:08:56 +0300

    Extract out ItemDetailDialog.

--- app/src/main/java/com/mukera/sheket/client/controller/items/ItemListFragment.java
+++ app/src/main/java/com/mukera/sheket/client/controller/items/ItemListFragment.java
@@ -1,39 +1,35 @@
 package com.mukera.sheket.client.controller.items;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.util.Pair;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.mukera.sheket.client.LoaderId;
 import com.mukera.sheket.client.R;
-import com.mukera.sheket.client.controller.util.NumberFormatter;
-import com.mukera.sheket.client.data.SheketContract.*;
+import com.mukera.sheket.client.controller.util.Utils;
+import com.mukera.sheket.client.data.SheketContract.CategoryEntry;
+import com.mukera.sheket.client.data.SheketContract.ItemEntry;
 import com.mukera.sheket.client.models.SBranch;
 import com.mukera.sheket.client.models.SBranchItem;
+import com.mukera.sheket.client.models.SCategory;
 import com.mukera.sheket.client.models.SItem;
 import com.mukera.sheket.client.utility.PrefUtil;
 
@@ -49,13 +45,13 @@ public class ItemListFragment extends Fragment implements LoaderCallbacks<Cursor
     private ListView mItemList;
     private ItemDetailAdapter mItemDetailAdapter;
 
-    private long mCategoryId;
+    private long mCategoryId = CategoryEntry.ROOT_CATEGORY_ID;
 
-    public static ItemListFragment newInstance(long cateogry_id) {
+    public static ItemListFragment newInstance(long category_id) {
         Bundle args = new Bundle();
 
         ItemListFragment fragment = new ItemListFragment();
-        args.putLong(KEY_CATEGORY_ID, cateogry_id);
+        args.putLong(KEY_CATEGORY_ID, category_id);
         fragment.setArguments(args);
 
         return fragment;
@@ -109,40 +105,72 @@ public class ItemListFragment extends Fragment implements LoaderCallbacks<Cursor
         return rootView;
     }
 
-    void resetAdapter() {
-        getLoaderManager().initLoader(LoaderId.MainActivity.ALL_ITEM_LOADER, null, this);
+    void startLoaders() {
+        getLoaderManager().initLoader(LoaderId.MainActivity.ITEM_LIST_LOADER, null, this);
+        getLoaderManager().initLoader(LoaderId.MainActivity.ITEM_LIST_CATEGORY_LOADER, null, this);
+    }
+
+    void restartLoaders() {
+        getLoaderManager().restartLoader(LoaderId.MainActivity.ITEM_LIST_LOADER, null, this);
+        getLoaderManager().restartLoader(LoaderId.MainActivity.ITEM_LIST_CATEGORY_LOADER, null, this);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        getLoaderManager().initLoader(LoaderId.MainActivity.ALL_ITEM_LOADER, null, this);
+        startLoaders();
         super.onActivityCreated(savedInstanceState);
     }
 
     @Override
     public Loader onCreateLoader(int id, Bundle args) {
-        String sortOrder = ItemEntry._full(ItemEntry.COLUMN_ITEM_ID) + " ASC";
+        long company_id = PrefUtil.getCurrentCompanyId(getContext());
+        switch (id) {
+            case LoaderId.MainActivity.ITEM_LIST_LOADER: {
+                String sortOrder = ItemEntry._full(ItemEntry.COLUMN_ITEM_ID) + " ASC";
 
-        return new CursorLoader(getActivity(),
-                ItemEntry.buildBaseUriWithBranches(PrefUtil.getCurrentCompanyId(getContext())),
-                SItem.ITEM_WITH_BRANCH_DETAIL_COLUMNS,
-                ItemEntry._full(ItemEntry.COLUMN_CATEGORY_ID) + " = ?",
-                new String[]{String.valueOf(mCategoryId)},
-                sortOrder
-        );
+                return new CursorLoader(getActivity(),
+                        ItemEntry.buildBaseUriWithBranches(company_id),
+                        SItem.ITEM_WITH_BRANCH_DETAIL_COLUMNS,
+                        ItemEntry._full(ItemEntry.COLUMN_CATEGORY_ID) + " = ?",
+                        new String[]{String.valueOf(mCategoryId)},
+                        sortOrder
+                );
+            }
+            case LoaderId.MainActivity.ITEM_LIST_CATEGORY_LOADER: {
+                String sortOrder = CategoryEntry._full(CategoryEntry.COLUMN_CATEGORY_ID) + " ASC";
+
+                return new CursorLoader(getActivity(),
+                        CategoryEntry.buildBaseUri(company_id),
+                        SCategory.CATEGORY_COLUMNS,
+                        CategoryEntry._full(CategoryEntry.COLUMN_PARENT_ID) + " = ?",
+                        new String[]{String.valueOf(mCategoryId)},
+                        sortOrder);
+            }
+        }
+        return null;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        mItemDetailAdapter.setItemCursor(data);
+        switch (loader.getId()) {
+            case LoaderId.MainActivity.ITEM_LIST_LOADER:
+                mItemDetailAdapter.setItemCursor(data);
+                break;
+            case LoaderId.MainActivity.ITEM_LIST_CATEGORY_LOADER:
+        }
     }
 
     @Override
     public void onLoaderReset(Loader loader) {
-        mItemDetailAdapter.setItemCursor(null);
+        switch (loader.getId()) {
+            case LoaderId.MainActivity.ITEM_LIST_LOADER:
+                mItemDetailAdapter.setItemCursor(null);
+                break;
+            case LoaderId.MainActivity.ITEM_LIST_CATEGORY_LOADER:
+        }
     }
 
-    static class SItemDetail {
+    public static class SItemDetail {
         public SItem item;
         public double total_quantity;
         public List<Pair<SBranch, SBranchItem>> available_branches;
@@ -193,11 +221,6 @@ public class ItemListFragment extends Fragment implements LoaderCallbacks<Cursor
             notifyDataSetChanged();
         }
 
-        private static final int[] colors = new int[] {
-                0xffffffff,
-                0xfff2f7ff
-        };
-
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             SItemDetail detail = getItem(position);
@@ -221,9 +244,7 @@ public class ItemListFragment extends Fragment implements LoaderCallbacks<Cursor
             } else {
                 holder.item_code.setVisibility(View.GONE);
             }
-            holder.total_qty.setText(NumberFormatter.formatDoubleForDisplay(detail.total_quantity));
-
-            convertView.setBackgroundColor(colors[position%2]);
+            holder.total_qty.setText(Utils.formatDoubleForDisplay(detail.total_quantity));
 
             return convertView;
         }
@@ -240,141 +261,4 @@ public class ItemListFragment extends Fragment implements LoaderCallbacks<Cursor
             }
         }
     }
-
-    public static class ItemDetailDialog extends DialogFragment {
-        public SItemDetail mItemDetail;
-
-        @NonNull
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-            LayoutInflater inflater = getActivity().getLayoutInflater();
-
-            View view = inflater.inflate(R.layout.dialog_all_item_detail, null);
-
-            ListView branchesList = (ListView) view.findViewById(R.id.dialog_all_item_list_view_branches);
-            DetailDialogAdapter adapter = new DetailDialogAdapter(getActivity());
-            adapter.setListener(new DetailDialogAdapter.BranchItemSelectionListener() {
-                @Override
-                public void editItemLocationSelected(final SBranchItem branchItem) {
-                    FragmentManager fm = getActivity().getSupportFragmentManager();
-
-                    final ItemLocationDialog dialog = new ItemLocationDialog();
-                    final Activity activity = getActivity();
-                    dialog.setBranchItem(branchItem);
-                    dialog.setListener(new ItemLocationDialog.ItemLocationListener() {
-                        @Override
-                        public void cancelSelected() {
-                            dialog.dismiss();
-                        }
-
-                        @Override
-                        public void locationSelected(final String location) {
-                            // the text didn't change, ignore it
-                            if (TextUtils.equals(branchItem.item_location, location))
-                                return;
-
-                            Thread t = new Thread() {
-                                @Override
-                                public void run() {
-                                    ContentValues values = new ContentValues();
-                                    values.put(BranchItemEntry.COLUMN_ITEM_LOCATION, location);
-
-                                    /**
-                                     * If the branch item was in created state, we don't want to change it until
-                                     * we sync with server. If we change it to updated state, it will create problems
-                                     * as the server still doesn't know about it and the update will fail.
-                                     */
-                                    if (branchItem.change_status != ChangeTraceable.CHANGE_STATUS_CREATED) {
-                                        values.put(ChangeTraceable.COLUMN_CHANGE_INDICATOR, ChangeTraceable.CHANGE_STATUS_UPDATED);
-                                    }
-                                    getContext().getContentResolver().update(
-                                            BranchItemEntry.buildBaseUri(PrefUtil.getCurrentCompanyId(getContext())),
-                                            values,
-                                            String.format("%s = ? AND %s = ?",
-                                                    BranchItemEntry.COLUMN_BRANCH_ID, BranchItemEntry.COLUMN_ITEM_ID),
-                                            new String[]{
-                                                    String.valueOf(branchItem.branch_id),
-                                                    String.valueOf(branchItem.item_id)
-                                            }
-                                    );
-                                    activity.runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            dialog.dismiss();
-                                        }
-                                    });
-                                }
-                            };
-                            t.start();
-                        }
-                    });
-                    dialog.show(fm, "Set Location");
-                }
-            });
-            for (Pair<SBranch, SBranchItem> pair : mItemDetail.available_branches) {
-                adapter.add(pair);
-            }
-            branchesList.setAdapter(adapter);
-
-            TextView qty_text_view = (TextView) view.findViewById(R.id.dialog_all_item_text_view_total_quantity);
-            qty_text_view.setText("Total Qty: " + NumberFormatter.formatDoubleForDisplay(mItemDetail.total_quantity));
-
-            return builder.setTitle(mItemDetail.item.name).
-                    setView(view).create();
-        }
-
-        public static class DetailDialogAdapter extends ArrayAdapter<Pair<SBranch, SBranchItem>> {
-            interface BranchItemSelectionListener {
-                void editItemLocationSelected(SBranchItem branchItem);
-            }
-
-            public BranchItemSelectionListener mListener;
-
-            public void setListener(BranchItemSelectionListener listener) {
-                mListener = listener;
-            }
-
-            public DetailDialogAdapter(Context context) {
-                super(context, 0);
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                final Pair<SBranch, SBranchItem> pair = getItem(position);
-
-                if (convertView == null) {
-                    LayoutInflater inflater = LayoutInflater.from(getContext());
-                    convertView = inflater.inflate(R.layout.list_item_all_item_detail_dialog, parent, false);
-                }
-
-                TextView branchName, itemLoc, itemQty;
-
-                branchName = (TextView) convertView.findViewById(R.id.list_item_all_item_item_detail_text_view_branch_name);
-                itemLoc = (TextView) convertView.findViewById(R.id.list_item_all_item_item_detail_text_view_location);
-                itemQty = (TextView) convertView.findViewById(R.id.list_item_all_item_item_detail_text_view_quantity);
-
-                branchName.setText(pair.first.branch_name);
-                SBranchItem branchItem = pair.second;
-                itemQty.setText(NumberFormatter.formatDoubleForDisplay(branchItem.quantity));
-                if (branchItem.item_location != null && !branchItem.item_location.isEmpty()) {
-                    itemLoc.setText(pair.second.item_location);
-                    itemLoc.setVisibility(View.VISIBLE);
-                } else {
-                    itemLoc.setVisibility(View.GONE);
-                }
-
-                ImageButton imgLocation = (ImageButton) convertView.findViewById(R.id.list_item_img_btn_all_item_detail_dialog);
-                imgLocation.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        mListener.editItemLocationSelected(pair.second);
-                    }
-                });
-
-                return convertView;
-            }
-        }
-    }
-
 }
