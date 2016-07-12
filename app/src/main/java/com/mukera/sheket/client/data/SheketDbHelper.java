02047816cbb9f82644ab7ac05673ad7948085ab6
Fuad Seffa <fuadjemal@gmail.com>
2016-07-13 02:11:18 +0300

    Fix !!!! HUGH !!!! BUG that was deleting ALL data when syncing. It was a problem with the way the database schema was setup, where there was a "ON CONFLICT REPLACE" constraint on the company id. And the company id is linked to by ALL other tables. So when we sync with a company, its id is always "re-added" to our database which causes the "CONFLICT REPLACE" to be executed resulting in the company row being deleted and re-added. When it is deleted, all referring tables, they all have "ON DELETE CASCADE" policy, also delete their data resulting in ALL of the data being deleted.

--- app/src/main/java/com/mukera/sheket/client/data/SheketDbHelper.java
+++ app/src/main/java/com/mukera/sheket/client/data/SheketDbHelper.java
@@ -46,7 +46,7 @@ public class SheketDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         final String sql_create_company_table = "create table if not exists " + CompanyEntry.TABLE_NAME + " ( " +
-                CompanyEntry.COLUMN_ID + " integer primary key ON CONFLICT REPLACE, " +
+                CompanyEntry.COLUMN_ID + " integer primary key ON CONFLICT IGNORE, " +
                 CompanyEntry.COLUMN_NAME + " text not null, " +
                 CompanyEntry.COLUMN_PERMISSION + " text not null, " +
                 // This can be empty because it might be the user's first time
