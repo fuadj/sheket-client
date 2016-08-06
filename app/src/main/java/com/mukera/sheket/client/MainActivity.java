9c8afe24ca4bad6c70b311778e1d6d68f725c070
Fuad Seffa <fuadjemal@gmail.com>
2016-08-06 10:04:37 +0300

    Remove the swipe right "ham-burger" menu.

--- app/src/main/java/com/mukera/sheket/client/MainActivity.java
+++ app/src/main/java/com/mukera/sheket/client/MainActivity.java
@@ -229,7 +229,7 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
