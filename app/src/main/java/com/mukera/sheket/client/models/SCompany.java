824e8c9758064b11d354950c4b1e7c26a3f423e6
Fuad Seffa <fuadjemal@gmail.com>
2016-08-31 09:12:57 +0300

    Fetch the user in company model.

--- app/src/main/java/com/mukera/sheket/client/models/SCompany.java
+++ app/src/main/java/com/mukera/sheket/client/models/SCompany.java
@@ -13,6 +13,7 @@ public class SCompany {
 
     public static final String[] COMPANY_COLUMNS = {
             f(CompanyEntry.COLUMN_COMPANY_ID),
+            f(CompanyEntry.COLUMN_USER_ID),
             f(CompanyEntry.COLUMN_NAME),
             f(CompanyEntry.COLUMN_PERMISSION),
             f(CompanyEntry.COLUMN_STATE_BACKUP),
@@ -21,15 +22,17 @@ public class SCompany {
     };
 
     public static final int COL_COMPANY_ID = 0;
-    public static final int COL_COMPANY_NAME = 1;
-    public static final int COL_PERMISSION = 2;
-    public static final int COL_STATE_BKUP = 3;
-    public static final int COL_PAYMENT_CERTIFICATE = 4;
-    public static final int COL_REMAINING_PERIOD = 5;
+    public static final int COL_USER_ID = 1;
+    public static final int COL_COMPANY_NAME = 2;
+    public static final int COL_PERMISSION = 3;
+    public static final int COL_STATE_BKUP = 4;
+    public static final int COL_PAYMENT_CERTIFICATE = 5;
+    public static final int COL_REMAINING_PERIOD = 6;
 
-    public static final int COL_LAST = 6;
+    public static final int COL_LAST = 7;
 
     public long company_id;
+    public long user_id;
     public String name;
     public String encoded_permission;
     public String state_bkup;
@@ -44,6 +47,7 @@ public class SCompany {
 
     public SCompany(Cursor cursor, int offset) {
         company_id = cursor.getLong(COL_COMPANY_ID + offset);
+        user_id = cursor.getLong(COL_USER_ID + offset);
         name = cursor.getString(COL_COMPANY_NAME + offset);
         encoded_permission = cursor.getString(COL_PERMISSION + offset);
         state_bkup = cursor.getString(COL_STATE_BKUP + offset);
@@ -54,6 +58,7 @@ public class SCompany {
     public ContentValues toContentValues() {
         ContentValues values = new ContentValues();
         values.put(CompanyEntry.COLUMN_COMPANY_ID, company_id);
+        values.put(CompanyEntry.COLUMN_USER_ID, user_id);
         values.put(CompanyEntry.COLUMN_NAME, name);
         values.put(CompanyEntry.COLUMN_PERMISSION, encoded_permission);
         values.put(CompanyEntry.COLUMN_STATE_BACKUP, state_bkup);
